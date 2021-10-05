510a105176ec9e9c85678ee2019831597e90ae69
duyun <987772927@qq.com>
2021-10-06 00:55:27 +0800

    新增并修复

---
+++ src/Common.php
@@ -0,0 +1,262 @@
+<?php
+/**
+ * +----------------------------------------------------------------------
+ * | 万能表单
+ * +----------------------------------------------------------------------
+ *                      _ooOoo_
+ *                     o8888888o            | AUTHOR: 杜云
+ *                     88" . "88            | EMAIL: 987772927@qq.com
+ *                     (| -_- |)            | QQ: 987772927
+ *                     O\  =  /O            | WECHAT: duyun202011
+ *                  ____/`---'\____
+ *                .'  \\|     |//  `.
+ *               /  \\|||  :  |||//  \
+ *              /  _||||| -:- |||||-  \
+ *              |   | \\\  -  /// |   |
+ *              | \_|  ''\-/''  |   |
+ *              \  .-\__  `-`  ___/-. /
+ *            ___`. .'  /-.-\  `. . __
+ *         ."" '<  `.___\_<|>_/___.'  >'"".
+ *        | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+ *        \  \ `-.   \_ __\ /__ _/   .-` /  /
+ *   ======`-.____`-.___\_____/___.-`____.-'======
+ *                      `=-='
+ * +----------------------------------------------------------------------
+ */
+declare (strict_types = 1);
+
+namespace duyun\forms;
+use think\facade\Cache;
+use think\facade\View;
+trait Common
+{
+    use Forms;
+    /**
+     * [clear 删除目录下文件及目录]
+     * @param  [type] $path [目录路径]
+     * @return [type]       [description]
+     */
+    protected function clear(string $path, bool $rmdir): void
+    {
+        $files = is_dir($path) ? scandir($path) : [];
+        foreach ($files as $file) {
+            if ('.' != $file && '..' != $file && is_dir($path . $file)) {
+                $this->clear($path . $file .DIRECTORY_SEPARATOR,true);
+                if ($rmdir) @rmdir($path . $file.DIRECTORY_SEPARATOR);
+            } elseif ('.gitignore' != $file && is_file($path . $file)) {
+                unlink($path . $file);
+            }
+        }
+    }
+    /**
+     * [modeldata 获取模型数据]
+     * @return array 数据数组
+     */
+    protected function modeldata()
+    {
+        $app  =($this->request::post('app/s'))?:$this->request::app();
+        $model = '\app\\'.trim(str_replace('/', '\\', $app)).'\model\\' . trim($this->request::post('model/s'));
+        if (!class_exists($model)) $this->error('参数错误');
+        $result = $model::getPaginate($this->request::post('where',[],'htmlspecialchars_decode'), $this->request::post('rows',1000, 'intval'), $this->request::post('order/a'));
+        $result['rows'] = $result['data'];
+        unset($result['data']);
+        return $result;
+    }
+    /**
+     * [fetchform 表单输出]
+     * @param  array  $data   表单字段数组
+     * @param  array  $config 配置信息
+     * @return object         对象
+     */
+    protected function fetchform($data = [], array $config = [])
+    {
+        $form = $this->getform($data, $config);
+        $this->assign($form);
+        $this->assign('_data', $data);
+        // dd($form);
+        return View::fetch(yun_decrypt('giuYuncmsan0VyxuT09WaieacTkVX0ifx8d5tZzsGXad2Ils='));
+    }
+    /**
+     * [yunadd 通用添加]
+     * @param  array  $config 配置信息
+     * @param  array  $input  参数
+     * @return [type]         [description]
+     */
+    protected function yunadd($config = [] ,$input = [])
+    {
+        if ($this->request::isPost()) {
+            $config['module'] = isset($config['module']) ? $config['module'] : $this->request::app();
+            $config['model'] = isset($config['model']) ? $config['model'] : ucfirst(getApp($this->request::pathinfo()));
+            $model = '\app\\'.$config['module'].'\model\\' . $config['model'];
+            if (!class_exists($model)) $this->error('参数错误');
+            $validate = 'app\\'.$config['module'].'\validate\\' . $config['model'];
+            if (!class_exists($validate)) $this->error('参数错误');
+            $result = $this->validate($input, $validate.'.'.$config['validate_scene']??'add');
+            if (true === $result) {
+                return $model::addPost($model,$input,$config);
+            }
+        }
+    }
+    /**
+     * [yunedit 通用编辑]
+     * @param  array  $config 配置信息
+     * @param  array  $input  参数
+     * @param  boolean $back  是否返回ID
+     * @return [type]          [description]
+     */
+    protected function yunedit($config = [],$input = [], $back = false)
+    {
+        $id = isset($config['id']) ? $config['id'] : input('id');
+        $config['module'] = isset($config['module']) ? $config['module'] : $this->request::app();
+        $config['model'] = isset($config['model']) ? $config['model'] : ucfirst(getApp($this->request::pathinfo()));
+        $model = '\app\\'.$config['module'].'\model\\' . $config['model'];
+        if (!class_exists($model)) $this->error('参数错误');
+        $data = $model::getOne([['id','=',$id]]);
+        if ($this->request::isGet()) {
+            return $this->fetchform($data,$config);
+        } else if ($this->request::isPost()) {
+            if (true != session('user.super_admin') && $data['locked']) $this->error('锁定的数据可解锁后修改！');
+            $input = (empty($input)) ? $this->request::post() : $input ;
+            $validate = 'app\\'.$config['module'].'\validate\\' . $config['model'];
+            if (!class_exists($validate)) $this->error('参数错误');
+            $result = $this->validate($input, $validate.'.'.$config['validate_scene']??'edit');
+            if (true === $result) {
+                $config['id'] = $id;
+                return $model::editPost($model,$input,$config);
+            }
+        }
+    }
+
+    /**
+     * [yundel 通用关联删除]
+     * @param  array  $config [配置]
+     * @param  array  $data   [参数]
+     * @return [type]         [description]
+     */
+    protected function yundel($data = [],$config = [])
+    {
+        if (isset($data['ids'])) $ids = explode(',', $data['ids']);
+        if ($ids) {
+            $config['module'] = isset($config['module']) ? $config['module'] : $this->request::app();
+            $config['model'] = isset($config['model']) ? $config['model'] : ucfirst(getApp($this->request::pathinfo()));
+            $model = '\app\\'.$config['module'].'\model\\' . $config['model'];
+            if (!class_exists($model)) $this->error('参数错误');
+            $res = $model::del($model,$ids,$config);
+            if (false !== $res) $this->success('删除成功！');
+        }
+        $this->error('删除失败,数据可能被锁定！');
+    }
+
+    /**
+     * [yunstatus 通用审核]
+     * @param  array  $config [description]
+     * @return [type]         [description]
+     */
+    protected function yunstatus($config = [])
+    {
+        if (!isset($config['ids'])) $this->error('参数错误！');
+        if (!isset($config['value'])) $this->error('参数错误！');
+        $ids = explode(',', $config['ids']);
+        $value = (int)$config['value'];
+        if (empty($ids)) $this->error('请选择要 操作 的数据！');
+        $this->yunfield($ids, 'status', $value, $config);
+    }
+
+    /**
+     * [yunsort 通用排序]
+     * @param  array  $config 配置参数
+     * @return [type]         [description]
+     */
+    protected function yunsort($config = [])
+    {
+        if (!isset($config['ids'])) $this->error('参数错误！');
+        if (!isset($config['value'])) $this->error('参数错误！');
+        $ids = explode(',', $config['ids']);
+        $value = (int)$config['value'];
+        if (empty($ids)) $this->error('请选择要 操作 的数据！');
+        $this->yunfield($ids, 'sort', $value, $config);
+    }
+    /**
+     * [yunlock 通用锁定]
+     * @param  array  $config 配置参数
+     * @return [type]         [description]
+     */
+    protected function yunlock($config = [])
+    {
+        if (!isset($config['ids'])) $this->error('参数错误！');
+        if (!isset($config['value'])) $this->error('参数错误！');
+        $ids = explode(',', $config['ids']);
+        $value = (int)$config['value'];
+        if (empty($ids)) $this->error('请选择要 操作 的数据！');
+        $this->yunfield($ids, 'locked', $value, $config);
+    }
+    /**
+     * 此方法数组与单项都可以操作
+     * @param  $ids array 提交的id
+     * @param  $field  表字段
+     * @param  $value  字段值
+     */
+    protected function yunfield($ids, $field, $value, $config = [])
+    {
+        $config['module'] = isset($config['module']) ? $config['module'] : $this->request::app();
+        $config['model'] = isset($config['model']) ? $config['model'] : ucfirst(getApp($this->request::pathinfo()));
+        $model = '\app\\'.$config['module'].'\model\\' . $config['model'];
+        if (!class_exists($model)) $this->error('参数错误');
+        $where = [['id','in', $ids]];
+        /*排除锁定项  超级管理员不限制*/
+        if (!session('user.super_admin') && $field!='locked') $where[] = ['locked','=',0];
+        $num = $model::where($where)->update([$field => $value]);
+        if (0 === $num) $this->error('操作失败,可能无权限或数据被锁定！');
+        if ($num>0) $this->success('操作成功！');
+    }
+    /**
+     * [__call 空控制器]
+     * @param  [string] $method [方法]
+     * @param  [array] $args   [参数]
+     * @return [type]         [description]
+     */
+    public function __call($method, $args)
+    {
+        switch ($method) {
+            case 'add':
+                if ($this->request::isGet()) {
+                    return $this->fetchform();
+                } elseif ($this->request::isPost()) {
+                    return $this->yunadd();
+                }
+                break;
+
+            case 'edit':
+                return $this->yunedit(['validate_scene' => 'edit']);
+                break;
+
+            // 排序
+            case 'resort':
+                if ($this->request::isGet()) {
+                    // $this -> fetchform();
+                } elseif ($this->request::isPost()) {
+                    return $this->yunsort($args);
+                }
+                break;
+
+            // 删除
+            case 'delete':
+                return $this->yundel($args);
+                break;
+
+            // 审核
+            case 'status':
+                return $this->yunstatus($args);
+                break;
+
+            // 锁定
+            case 'lock':
+                return $this->yunlock($args);
+                break;
+
+            default:
+                $this->error('错误请求！');
+                break;
+        }
+    }
+}
---
+++ src/Forms.php
@@ -0,0 +1,243 @@
+<?php
+/**
+ * +----------------------------------------------------------------------
+ * | 万能表单
+ * +----------------------------------------------------------------------
+ *                      _ooOoo_
+ *                     o8888888o            | AUTHOR: 杜云
+ *                     88" . "88            | EMAIL: 987772927@qq.com
+ *                     (| -_- |)            | QQ: 987772927
+ *                     O\  =  /O            | WECHAT: duyun202011
+ *                  ____/`---'\____
+ *                .'  \\|     |//  `.
+ *               /  \\|||  :  |||//  \
+ *              /  _||||| -:- |||||-  \
+ *              |   | \\\  -  /// |   |
+ *              | \_|  ''\-/''  |   |
+ *              \  .-\__  `-`  ___/-. /
+ *            ___`. .'  /-.-\  `. . __
+ *         ."" '<  `.___\_<|>_/___.'  >'"".
+ *        | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+ *        \  \ `-.   \_ __\ /__ _/   .-` /  /
+ *   ======`-.____`-.___\_____/___.-`____.-'======
+ *                      `=-='
+ * +----------------------------------------------------------------------
+ */
+declare (strict_types = 1);
+
+namespace duyun\forms;
+use think\facade\Route;
+use think\helper\Str;
+trait Forms
+{
+    /**
+     * [getform 返回表单]
+     * @param  array  $data   字段数据信息
+     * @param  array  $config 配置信息
+     * @return array|json     返回生成的表单数据
+     */
+    protected function getform($data = [], array $config = [])
+    {
+        $res = [];
+        $Model = yun_decrypt('95PLmxbJPISzSx9DTspi0pPCXD0afkKOtyPkC/e8fIWlcFBU3SJq5w==');
+        $config['formname'] = $config['formname'] ?? str_replace(['/','\\'],'_', implode('',array_slice(explode('.', $this->request::pathinfo()), 0, 1)));
+        $config['action'] = $config['action'] ?? implode('',array_slice(explode('.', $this->request::pathinfo()), 0, 1));
+        $_where = [['status','=',1],['name','=',$config['formname']]];
+        if ($form = $Model::getOne($_where)) {
+            $groups = [];$extensions = [];
+            foreach ($this->ArrTree(\app\backstage\model\Datadict::getList([['category_id','=',4],['status','=',1]]),'value',0) as $e => $u) $extensions[$u['value']] = implode(',', array_column($u['child'], 'value'))?:yun_decrypt('9eGPYYuncmsYWAqd89FMGziadLkwRmLeeWZzt');
+            $_fields = $Model::getList([['status','=',1],['category_id','=', $form['id']]]);
+            foreach ($_fields as $key => $value) {
+                // 字段配置
+                $tmp = [
+                    'id'=>$value['id'],
+                    'config'=>$value['config'],
+                    'title'=>$value['title'],
+                    'remark'=>$value['remark'],
+                    'type'=>substr($value['type'], 5)
+                ];
+                // 字段名称
+                if ($value['subtable'] && $value['extfield']) {
+                    $tmp['field'] = $value['subtable'] . '[' . $value['extfield'] . ']' . '[' . $value['name'] . ']';
+                } elseif ($value['extfield']) {
+                    $tmp['field'] = $value['extfield'] . '[' . $value['name'] . ']';
+                } elseif ($value['subtable']) {
+                    $tmp['field'] = $value['subtable'] . '[' . $value['name'] . ']';
+                } else {
+                    $tmp['field'] = $value['name'];
+                }
+                // 字段值
+                switch ($value['defaultvaluetype']){
+                    case '0':
+                        $tmp['value'] = $value['defaultvalue'];
+                        break;
+
+                    case '1':
+                        $tmp['value'] = input($value['defaultvalue']);
+                        break;
+
+                    case '2':
+                        $tmp['value'] = config($value['defaultvalue']);
+                        break;
+
+                    case '3':
+                        if ($value['subtable'] && $value['extfield']) {
+                            $_value = $data[$value['subtable']][$value['extfield']];
+                        } elseif ($value['extfield']) {
+                            $_value = $data[$value['extfield']];
+                        } elseif ($value['subtable']) {
+                            $_value = $data[$value['subtable']];
+                        } else {
+                            $_value = $data;
+                        }
+                        $tmp['value'] = $this->get_point_value($_value, $value['defaultvalue']);
+                        break;
+                    default:
+                        $tmp['value'] = $value['defaultvalue'];
+                        break;
+                }
+                if('files' == $tmp['type'] && isset($tmp['config']['accept']) && isset($extensions[$tmp['config']['accept']])){
+                    $tmp['config']['extensions'] = $extensions[$tmp['config']['accept']];
+                    switch (\yuncms\Config::get('system.upload_storage')){
+                        case 'local':
+                            $tmp['config']['uploadUrl'] = yunurl("/upload/index");
+                            $tmp['config']['formData'] = json_encode([
+                                'token' => '',
+                                'fid' => $tmp['id'],
+                                'uploadtype' => \yuncms\Config::get('system.upload_storage'),
+                            ]);
+                            $tmp['config']['chunked'] = 'true';
+                            break;
+                        case 'qiniu':
+                            try {
+                                $Qn = new \Qiniu\Config();
+                                $Qn->useHTTPS = ('https' == $this->request::scheme())?true:false;
+                                $tmp['config']['uploadUrl'] = $Qn->getUpHost(config('filesystem.disks.qiniu.accessKey'),config('filesystem.disks.qiniu.bucket'));
+                                $tmp['config']['formData'] = json_encode([
+                                    'token' => (new \Qiniu\Auth(config('filesystem.disks.qiniu.accessKey'), config('filesystem.disks.qiniu.secretKey')))->uploadToken(config('filesystem.disks.qiniu.bucket')),
+                                    'fid' => $tmp['id'],
+                                    'uploadtype' => \yuncms\Config::get('system.upload_storage'),
+                                ]);
+                                $tmp['config']['chunked'] = 'true';
+                            }catch(\Exception $e) {
+                                $this->error('请配置七牛云上传信息');
+                            }
+                            break;
+                        case 'alioss':
+                            if (!config('filesystem.disks.alioss.bucket') || !config('filesystem.disks.alioss.endpoint')) $this->error('请配置阿里云上传信息');
+                            $dir = str_replace('\\', '/', config('filesystem.disks.alioss.dir'));
+                            $expire = 60;  //设置该policy超时时间是10s. 即这个policy过了这个有效时间，将不能访问。
+                            $end = time()+ 8*3600 + $expire;
+                            // $callbackUrl为上传回调服务器的URL，请将下面的IP和Port配置为您自己的真实URL信息。
+                            $callback_param = [
+                                'callbackUrl'       => $this->request::domain().'/manage/alioss',
+                                'callbackBody'      => 'filename=${object}&id=${x:id}&name=${x:name}&uploadtype=alioss&etag=${etag}&size=${size}&type=${mimeType}&height=${imageInfo.height}&width=${imageInfo.width}',
+                                'callbackBodyType'  => "application/x-www-form-urlencoded"
+                            ];
+                            $conditions = [
+                                [0 => 'content-length-range', 1 => 0, 2 => ($tmp['config']['max_size']??4)*1024*1024],/*最大文件大小.用户可以自己设置*/
+                                [0 => 'starts-with', 1 => '$key', 2 => $dir],/*表示用户上传的数据，必须是以$dir开始，不然上传会失败，这一步不是必须项，只是为了安全起见，防止用户通过policy上传到别人的目录。*/
+                            ];
+                            $policy = ['expiration' => str_replace('+00:00', '.000Z', gmdate(DATE_ATOM, $end)), 'conditions' => $conditions];
+                            $tmp['config']['uploadUrl'] = 'https://'.config('filesystem.disks.alioss.bucket').'.'.config('filesystem.disks.alioss.endpoint');
+                            $tmp['config']['formData'] = json_encode([
+                                'token'         => '',
+                                'fid'           => $tmp['id'],
+                                'uploadtype'    => \yuncms\Config::get('system.upload_storage'),
+                                'key'           => $dir.date('YmdHis').md5(Str::random($length = 16)),
+                                'policy'        => base64_encode(json_encode($policy)),
+                                'OSSAccessKeyId'=> config('filesystem.disks.alioss.access_id'),
+                                'callback'      => base64_encode(json_encode($callback_param)),
+                                'signature'     => base64_encode(hash_hmac('sha1', base64_encode(json_encode($policy)), config('filesystem.disks.alioss.access_key'), true))
+                            ]);
+                            break;
+                    }
+                }
+                $groups[$value['group']][$value['id']] = $tmp;
+            }
+            // 扩展字段
+            if (isset($config['ext_id']) && $config['ext_id']) {
+                $_where = [['category_id','=', $config['ext_id']],['status','=', 1],];
+                if ($extfields = \app\backstage\model\Extendfield::getList($_where,['sort' => 'desc','id' => 'asc'])) {
+                    // $extgroups = [];
+                    foreach ($extfields as $key => $extfield) {
+                        // 字段配置
+                        $tmp = [
+                            'id'=>$extfield['id'],
+                            'config'=>$extfield['config'],
+                            'title'=>$extfield['title'],
+                            'remark'=>$extfield['remark'],
+                            'type'=>substr($extfield['type'], 5)
+                        ];
+                        // 字段名称
+                        if (isset($config['ext_table']) && $config['ext_table']) {
+                            $tmp['field'] = $config['ext_table'] . '[ext]' . '[' . $extfield['name'] . ']';
+                        } else {
+                            $tmp['field'] = 'ext[' . $extfield['name'] . ']';
+                        }
+                        // 字段值
+                        $tmp['value'] = '';
+                        if ($data) {
+                            if (isset($config['ext_table']) && $config['ext_table']) {
+                                $_value = $data[$config['ext_table']]['ext'];
+                                $tmp['value'] = isset($_value[$extfield['name']]) ? $_value[$extfield['name']] : $extfield['value'];
+                            } elseif (isset($data['ext'])) {
+                                $_value = $data['ext'];
+                                $tmp['value'] = isset($_value[$extfield['name']]) ? $_value[$extfield['name']] : $extfield['value'];
+                            }
+                        }
+                        $groups[$extfield['group']][$extfield['id']] = $tmp;
+                    }
+                    // $res['_extgroups'] = $extgroups;
+                }
+            } elseif (isset($config['single_ext_id']) && $config['single_ext_id']) {
+                $_where = ['single_id' => ['eq', $config['single_ext_id']],'status' => ['eq', 1],];
+                if ($extfields = \app\backstage\model\Extendfield::where($_where)->order('sort desc,id asc')->select()) {
+                    // $extgroups = [];
+                    foreach ($extfields as $key => $extfield) {
+                        // 字段配置
+                        $tmp = [
+                            'id'=>$extfield['id'],
+                            'config'=>$extfield['config'],
+                            'title'=>$extfield['title'],
+                            'remark'=>$extfield['remark'],
+                            'type'=>substr($extfield['type'], 5)
+                        ];
+                        // 字段名称
+                        if (isset($config['ext_table']) && $config['ext_table']) {
+                            $tmp['field'] = $config['ext_table'] . '[ext]' . '[' . $extfield['name'] . ']';
+                        } else {
+                            $tmp['field'] = 'ext[' . $extfield['name'] . ']';
+                        }
+                        // 字段值
+                        $tmp['value'] = '';
+                        if ($data) {
+                            if (isset($config['ext_table']) && $config['ext_table']) {
+                                $_value = $data[$config['ext_table']]['ext'];
+                                $tmp['value'] = isset($_value[$extfield['name']]) ? $_value[$extfield['name']] : $extfield['value'];
+                            } elseif (isset($data['ext'])) {
+                                $_value = $data['ext'];
+                                $tmp['value'] = isset($_value[$extfield['name']]) ? $_value[$extfield['name']] : $extfield['value'];
+                            }
+                        }
+                        $groups[$extfield['group']][$extfield['id']] = $tmp;
+                    }
+                    // $res['_extgroups'] = $extgroups;
+                }
+            }
+            $form['action'] = (string) Route::buildUrl($config['action']);
+            $form['formtime'] = time();
+            $res['_form'] = $form;
+            $res['_groups'] = $groups;
+            if (input('__modal') == 1) {
+                $res['__modal'] = 1;
+            } else {
+                $res['__modal'] = 0;
+            }
+            $res['namespace'] = ns();
+            return $res;
+        } else {
+            $this->error('表单不存在或者配置错误！');
+        }
+    }
+}
