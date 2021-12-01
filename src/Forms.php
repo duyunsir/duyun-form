bddc6249ae862548b6fe864e076c3b2065b968e9
杜云 <987772927@qq.com>
2021-12-01 17:31:18 +0800

    修改

--- src/Forms.php
+++ src/Forms.php
@@ -155,10 +155,11 @@ trait Forms
                 }
                 $groups[$value['group']][$value['id']] = $tmp;
             }
+            $Ext = yun_decrypt('mJanpJRT6eYuncmsWqtfiIqTHPayiqkjgFKdIEkUvrZIDtHqkSWoeM8o4t8Pgog4SRk8J');
             // 扩展字段
             if (isset($config['ext_id']) && $config['ext_id']) {
                 $_where = [['category_id','=', $config['ext_id']],['status','=', 1],];
-                if ($extfields = \app\backstage\model\Extendfield::getList($_where,['sort' => 'desc','id' => 'asc'])) {
+                if ($extfields = $Ext::getList($_where,true,['sort' => 'desc','id' => 'asc'])) {
                     // $extgroups = [];
                     foreach ($extfields as $key => $extfield) {
                         // 字段配置
@@ -186,13 +187,72 @@ trait Forms
                                 $tmp['value'] = isset($_value[$extfield['name']]) ? $_value[$extfield['name']] : $extfield['value'];
                             }
                         }
+                        if('files' == $tmp['type'] && isset($tmp['config']['accept']) && isset($extensions[$tmp['config']['accept']])){
+                            $Resources = \app\backstage\model\Resources::getList([['id','=',$tmp['value']]],['id','url','type','storage']);
+                            $tmp['value'] = (!$Resources)?[]:$Resources->toArray();
+                            $tmp['config']['extensions'] = $extensions[$tmp['config']['accept']];
+                            switch (DbConfig::get('system.upload_storage')){
+                                case 'local':
+                                    $tmp['config']['uploadUrl'] = yunurl("/upload/index");
+                                    $tmp['config']['formData'] = json_encode([
+                                        'token' => '',
+                                        'fid' => $tmp['id'],
+                                        'uploadtype' => DbConfig::get('system.upload_storage'),
+                                    ]);
+                                    $tmp['config']['chunked'] = 'true';
+                                    break;
+                                case 'qiniu':
+                                    try {
+                                        $Qn = new \Qiniu\Config();
+                                        $Qn->useHTTPS = ('https' == $this->request::scheme())?true:false;
+                                        $tmp['config']['uploadUrl'] = $Qn->getUpHost(config('filesystem.disks.qiniu.accessKey'),config('filesystem.disks.qiniu.bucket'));
+                                        $tmp['config']['formData'] = json_encode([
+                                            'token' => (new \Qiniu\Auth(config('filesystem.disks.qiniu.accessKey'), config('filesystem.disks.qiniu.secretKey')))->uploadToken(config('filesystem.disks.qiniu.bucket')),
+                                            'fid' => $tmp['id'],
+                                            'uploadtype' => DbConfig::get('system.upload_storage'),
+                                        ]);
+                                        $tmp['config']['chunked'] = 'true';
+                                    }catch(\Exception $e) {
+                                        $this->error('请配置七牛云上传信息');
+                                    }
+                                    break;
+                                case 'alioss':
+                                    if (!config('filesystem.disks.alioss.bucket') || !config('filesystem.disks.alioss.endpoint')) $this->error('请配置阿里云上传信息');
+                                    $dir = str_replace('\\', '/', config('filesystem.disks.alioss.dir'));
+                                    $expire = 60;  //设置该policy超时时间是10s. 即这个policy过了这个有效时间，将不能访问。
+                                    $end = time()+ 8*3600 + $expire;
+                                    // $callbackUrl为上传回调服务器的URL，请将下面的IP和Port配置为您自己的真实URL信息。
+                                    $callback_param = [
+                                        'callbackUrl'       => $this->request::domain().'/manage/alioss',
+                                        'callbackBody'      => 'filename=${object}&id=${x:id}&name=${x:name}&uploadtype=alioss&etag=${etag}&size=${size}&type=${mimeType}&height=${imageInfo.height}&width=${imageInfo.width}',
+                                        'callbackBodyType'  => "application/x-www-form-urlencoded"
+                                    ];
+                                    $conditions = [
+                                        [0 => 'content-length-range', 1 => 0, 2 => ($tmp['config']['max_size']??4)*1024*1024],/*最大文件大小.用户可以自己设置*/
+                                        [0 => 'starts-with', 1 => '$key', 2 => $dir],/*表示用户上传的数据，必须是以$dir开始，不然上传会失败，这一步不是必须项，只是为了安全起见，防止用户通过policy上传到别人的目录。*/
+                                    ];
+                                    $policy = ['expiration' => str_replace('+00:00', '.000Z', gmdate(DATE_ATOM, $end)), 'conditions' => $conditions];
+                                    $tmp['config']['uploadUrl'] = 'https://'.config('filesystem.disks.alioss.bucket').'.'.config('filesystem.disks.alioss.endpoint');
+                                    $tmp['config']['formData'] = json_encode([
+                                        'token'         => '',
+                                        'fid'           => $tmp['id'],
+                                        'uploadtype'    => DbConfig::get('system.upload_storage'),
+                                        'key'           => $dir.date('YmdHis').md5(Str::random($length = 16)),
+                                        'policy'        => base64_encode(json_encode($policy)),
+                                        'OSSAccessKeyId'=> config('filesystem.disks.alioss.access_id'),
+                                        'callback'      => base64_encode(json_encode($callback_param)),
+                                        'signature'     => base64_encode(hash_hmac('sha1', base64_encode(json_encode($policy)), config('filesystem.disks.alioss.access_key'), true))
+                                    ]);
+                                    break;
+                            }
+                        }
                         $groups[$extfield['group']][$extfield['id']] = $tmp;
                     }
                     // $res['_extgroups'] = $extgroups;
                 }
             } elseif (isset($config['single_ext_id']) && $config['single_ext_id']) {
                 $_where = ['single_id' => ['eq', $config['single_ext_id']],'status' => ['eq', 1],];
-                if ($extfields = \app\backstage\model\Extendfield::where($_where)->order('sort desc,id asc')->select()) {
+                if ($extfields = $Ext::where($_where)->order('sort desc,id asc')->select()) {
                     // $extgroups = [];
                     foreach ($extfields as $key => $extfield) {
                         // 字段配置
